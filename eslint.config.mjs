56363467ce0d1f6407b389b82ba6118703f9cac6
Zhenya Peeva <jenny.radulova@gmail.com>
2026-09-21 09:25:32 +0300

    chore: Update project setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,23 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { 
-    files: ["**/*.{js,mjs,cjs}"], 
-    plugins: { js }, 
-    extends: ["js/recommended"], 
-    languageOptions: { 
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.node,
       },
+    },
+  },
+  //Disabling `no-empty-pattern` for test files
+  {
+    files: ["tests/fixtures/**/*.js"],
+    rules: {
+      "no-empty": "off",
+      "no-empty-pattern": "off",
     },
   },
 ]);
--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
+    /*
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
@@ -42,6 +42,7 @@ export default defineConfig({
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
+  */
   ],
 
   outputDir: "test-results",
